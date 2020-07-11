89e7494eca16246c2de36f42bf9da067ef61482e
Yoodahun <dahun4032@gmail.com>
2020-07-12 01:02:03 +0900

    first commit

---
+++ src/test/java/DataDriven.java
@@ -0,0 +1,67 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class DataDriven {
+
+    public ArrayList<String> getData() {
+        FileInputStream fis = null;
+        XSSFWorkbook workbook = null;
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        try {
+            fis = new FileInputStream("Testcase.xlsx");
+            workbook = new XSSFWorkbook(fis);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(workbook.getNumberOfSheets());
+
+        XSSFSheet sheet = workbook.getSheet("TestSheet");
+        //Identify Testcases column by scanning the entire 1st row
+
+        Iterator<Row> rows = sheet.iterator();
+        Row row = rows.next();
+
+        Iterator<Cell> cells = row.cellIterator();
+
+        int rownumber = 0;
+        while (cells.hasNext()) {
+            Cell cell = cells.next();
+            if(cell.getStringCellValue().equals("Testcase")) {
+                //catch column
+                rownumber = cell.getColumnIndex();
+                break;
+            }
+        }
+        //Once column is indentified then, scan entire testcase column to identify purchase testcase row
+
+        while(rows.hasNext()) {
+            row = rows.next();
+            if(row.getCell(rownumber).getStringCellValue().equals("Purchase")) {
+                cells = row.cellIterator();
+                break;
+            }
+        }
+
+        while (cells.hasNext()) {
+            Cell cell = cells.next();
+            if(cell.getCellType() == CellType.STRING) {
+                arrayList.add(cell.getStringCellValue());
+            } else if (cell.getCellType() == CellType.NUMERIC) {
+                arrayList.add(NumberToTextConverter.toText(cell.getNumericCellValue()));
+            }
+
+        }
+
+        return arrayList;
+    }
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSample {
+    public static void main(String[] args) {
+        DataDriven dataDriven = new DataDriven();
+
+        List<String> list = dataDriven.getData();
+
+        for (String data : list) {
+            System.out.println(data);
+        }
+
+
+    }
+}
